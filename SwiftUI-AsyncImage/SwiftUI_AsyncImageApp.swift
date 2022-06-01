d12ccba4f91de67758ce9ac2116b49659ec4acdd
Matthew Wei <matthewwei35@gmail.com>
2022-05-31 18:43:41 -0700

    Initial Commit

---
+++ SwiftUI-AsyncImage/SwiftUI_AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AsyncImageApp.swift
+//  SwiftUI-AsyncImage
+//
+//  Created by Matthew Wei on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
